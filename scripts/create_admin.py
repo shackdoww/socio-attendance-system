1d9d2cc56455ac88d4c613fd22e55f357e74ad7d
Jezryll Philip N. Tamse <137472986+shackdoww@users.noreply.github.com>
2026-09-11 21:06:11 +0800

    Fix admin setup script import path

--- scripts/create_admin.py
+++ scripts/create_admin.py
@@ -1,4 +1,8 @@
+import os
+import sys
 from getpass import getpass
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from app import app
 from extensions import db
