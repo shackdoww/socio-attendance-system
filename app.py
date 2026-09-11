54a8c237656a59771eb73a81760816d3587a2f4f
Jezryll Philip N. Tamse <137472986+shackdoww@users.noreply.github.com>
2026-09-11 21:00:03 +0800

    Fix dashboard redirect imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_login import LoginManager, current_user, login_required
 from dotenv import load_dotenv
 import os
