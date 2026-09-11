fce494de6993b39e4290772016884ac1b26aad27
Jezryll Philip N. Tamse <137472986+shackdoww@users.noreply.github.com>
2026-09-12 01:01:09 +0800

    Fix SQLAlchemy import syntax error

--- routes/admin.py
+++ routes/admin.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
-from sqlalchemy import or
+from sqlalchemy import or_
 
 from extensions import db
 from models import Socio, User
