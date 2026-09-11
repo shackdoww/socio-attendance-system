4c1f7041d4365f097d1a241f65256ff91187ab87
Jezryll Philip N. Tamse <137472986+shackdoww@users.noreply.github.com>
2026-09-11 21:48:13 +0800

    Fix bulletin current user import

--- routes/bulletin.py
+++ routes/bulletin.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import login_required
+from flask_login import current_user, login_required
 
 from extensions import db
 from models import BulletinPost
