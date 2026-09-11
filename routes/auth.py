ccb776cf253ed1aa30d63cce0efb0177379dc4f2
Jezryll Philip N. Tamse <137472986+shackdoww@users.noreply.github.com>
2026-09-11 21:02:29 +0800

    Fix authentication database import

--- routes/auth.py
+++ routes/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required, login_user, logout_user
 
-from app import db
+from extensions import db
 from models import User
 
 auth_bp = Blueprint("auth", __name__)
