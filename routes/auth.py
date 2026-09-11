72f24349f96eca495d27bf7077e48ed29b213801
Jezryll Philip N. Tamse <137472986+shackdoww@users.noreply.github.com>
2026-09-12 00:54:42 +0800

    Make logout a CSRF-protected POST action

--- routes/auth.py
+++ routes/auth.py
@@ -32,7 +32,7 @@ def login():
     return render_template("auth/login.html")
 
 
-@auth_bp.route("/logout")
+@auth_bp.route("/logout", methods=["POST"])
 @login_required
 def logout():
     logout_user()
